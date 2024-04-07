18a064f01b98629d0ead323b524591c08adc6132
Bryan De Lama <bryan.delama@utec.edu.pe>
2024-04-07 15:07:19 -0500

    Added 2nd scatter

--- app.py
+++ app.py
@@ -7,34 +7,29 @@ car_data = pd.read_csv('vehicles_us.csv') # leer los datos
 #Header
 st.header("Sprint 5: Herramientas de desarrollo de software")
 
-#hist_button = st.button('Construir histograma') # crear un botón
-
-#if hist_button: # al hacer clic en el botón
-    # escribir un mensaje
-#    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-    
-    # crear un histograma
-#    fig = px.histogram(car_data, x="odometer")
-
-    # mostrar un gráfico Plotly interactivo
-#    st.plotly_chart(fig, use_container_width=True)
-
 # Casillas de verificación para elegir el tipo de gráfico
 option_histogram = st.checkbox('Histograma')
 option_scatter = st.checkbox('Gráfico de Dispersión')
 
 # Generar gráfico basado en la selección del usuario
 if option_histogram:
-    st.subheader("Histograma")
+    #Primer histograma
+    st.subheader("Gráfico de Histograma")
     fig_hist_1 = px.histogram(car_data, x='price', title='Distribución de Precios')
-    st.plotly_chart(fig_hist_1)
+    st.plotly_chart(fig_hist_1, use_container_width=True)
     #Segundo histograma
     fig_hist_2 = px.histogram(car_data, x="odometer", title='Distribución del Odómetro')
     st.plotly_chart(fig_hist_2, use_container_width=True)
 
 if option_scatter:
-    st.subheader("Gráfico de Dispersión: Precio vs Año del Modelo")
-    fig_scatter = px.scatter(car_data, x='model_year', y='price', 
+    #Primer gráfico de dispersión
+    st.subheader("Gráfico de Dispersión")
+    fig_scatter_1 = px.scatter(car_data, x='model_year', y='price', 
                              title='Precio vs Año del Modelo',
                              labels={'model_year': 'Año del Modelo', 'price': 'Precio'})
-    st.plotly_chart(fig_scatter)
+    st.plotly_chart(fig_scatter_1)
+    #Segundo gráfico de dispersión
+    fig_scatter_2 = px.scatter(car_data, x='days_listed', y='odometer', 
+                                title='Odómetro vs Días en lista',
+                                labels={'days_listed': 'Días en lista', 'odometer': 'Odómetro'})
+    st.plotly_chart(fig_scatter_2)
